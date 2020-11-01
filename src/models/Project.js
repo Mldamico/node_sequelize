146c276078a787dc5e8bb28a2672a2b744e44795
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-11-01 12:53:20 -0300

    Tasks model

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,23 +1,29 @@
 import Sequelize from 'sequelize';
 import { sequelize } from '../database/database';
 
-const Project = sequelize.define('projects', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
+const Project = sequelize.define(
+  'projects',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.TEXT,
+    },
+    priority: {
+      type: Sequelize.INTEGER,
+    },
+    description: {
+      type: Sequelize.TEXT,
+    },
+    deliverydate: {
+      type: Sequelize.DATE,
+    },
   },
-  name: {
-    type: Sequelize.TEXT,
-  },
-  priority: {
-    type: Sequelize.INTEGER,
-  },
-  description: {
-    type: Sequelize.TEXT,
-  },
-  deliverydate: {
-    type: Sequelize.DATE,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 export default Project;
--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -1,2 +1,26 @@
 import Sequelize from 'sequelize';
 import { sequelize } from '../database/database';
+
+const Tasks = sequelize.define(
+  'tasks',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.TEXT,
+    },
+    done: {
+      type: Sequelize.BOOLEAN,
+    },
+    projectId: {
+      type: Sequelize.INTEGER,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export default Tasks;
